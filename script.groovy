05daf8bc4449b61ec39dd5c8194a2d375efed5bc
“iimnurd” <iim@alterra.id>
2020-09-21 09:42:18 +0700

    test

--- script.groovy
+++ script.groovy
@@ -1,4 +1,4 @@
 node {
-  stage 'aa Run JMeter Test iim'
+  stage 'bb Run JMeter Test iim'
   sh '/Users/alterra/Desktop/apache-jmeter-5.1.1/bin/jmeter -Jthreads=5 -n -t /Users/alterra/Desktop/test_google.jmx  -l /Users/alterra/Desktop/jenkin_report/5.csv -j /Users/alterra/Desktop/5.log'
 }
