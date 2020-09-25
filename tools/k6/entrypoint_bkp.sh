6a071fb57385a493e28b065fad6208bf6ecbf593
“iimnurd” <iim@alterra.id>
2020-09-25 14:06:44 +0700

    test

---
+++ tools/k6/entrypoint_bkp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# `$*` expands the `args` supplied in an `array` individually 
+# or splits `args` in a string separated by whitespace.
+echo "Execution is being started"
+echo "**************************"
+export K6_CLOUD_TOKEN=$INPUT_TOKEN
+export K6_COMMAND=$([[ $INPUT_CLOUD == "true" ]] && echo 'cloud' || echo 'run')
+sh -c "k6 $K6_COMMAND --config $INPUT_CONFIG  $INPUT_FILENAME $INPUT_FLAGS"
+echo "**************************"
+echo "Execution has been completed, please check the artifacts to download the results."
